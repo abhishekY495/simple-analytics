0dadafbfe449dda9e7200d8cb29bd3d97d200b69
Abhishek <abhishekY495.sec@gmail.com>
2026-02-25 15:55:09 +0530

    init

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col p-2">
-      <h1 className="text-center text-3xl font-bold">Simple Analytics</h1>
-    </div>
-  );
-}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col p-5 items-center">
+      <h1 className="text-4xl font-semibold">Simple Analytics</h1>
+    </div>
+  );
+}

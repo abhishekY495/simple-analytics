c13c0530eca3d1638c15db947166e14ffad50c5a
Abhishek <abhishekY495.sec@gmail.com>
2026-02-26 16:48:57 +0530

    Add .air.toml configuration and implement environment variable loading

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module github.com/abhishekY495/simple-analytics/backend
 
 go 1.25.0
 
-require github.com/go-chi/chi/v5 v5.2.5
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/joho/godotenv v1.5.1
+)
--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServerPort  string
+	DatabaseUrl string
+}
+
+func LoadConfig() (Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return Config{}, fmt.Errorf("Failed to load .env")
+	}
+
+	databaseUrl, err := extractEnv("DATABASE_URL")
+	if err != nil {
+		return Config{}, err
+	}
+	serverPort, err := extractEnv("SERVER_PORT")
+	if err != nil {
+		return Config{}, err
+	}
+
+	return Config{
+		ServerPort:  serverPort,
+		DatabaseUrl: databaseUrl,
+	}, nil
+}
+
+func extractEnv(key string) (string, error) {
+	val := os.Getenv(key)
+	if val == "" {
+		return "", fmt.Errorf("requested env not found")
+	}
+	return val, nil
+}

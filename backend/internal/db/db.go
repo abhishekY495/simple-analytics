65fa9e47a0ecbe8a84ca0c9448ddc569d5a2e07b
Abhishek <abhishekY495.sec@gmail.com>
2026-02-27 21:18:35 +0530

    Enhance database connection handling with improved error logging and configuration parsing

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -10,11 +10,26 @@ import (
 
 func Connect(cfg config.Config) (*pgxpool.Pool, error) {
 	ctx := context.Background()
+	config, err := pgxpool.ParseConfig(cfg.DatabaseUrl)
+
+	if err != nil {
+		log.Printf("Unable to parse database URL: %v", err)
+		return nil, err
+	}
 
 	// Connect to the database
-	pool, err := pgxpool.New(ctx, cfg.DatabaseUrl)
+	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
-		log.Fatalf("Unable to connect to database: %v", err)
+		log.Printf("Unable to connect to database: %v", err)
+		pool.Close()
+		return nil, err
+	}
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		log.Printf("Unable to ping database: %v", err)
+		pool.Close()
+		return nil, err
 	}
 
 	log.Println("Connection established")

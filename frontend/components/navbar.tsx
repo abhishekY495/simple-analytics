df9306431bc58d2b48f3e60aa34b90b83351a51e
abhishekY495 <abhishekY495.sec@gmail.com>
2026-03-03 20:45:41 +0530

    styling

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Navbar() {
   return (
     <nav className="flex items-center justify-center py-2">
-      <Link href="/" className="flex items-center gap-2 p-3 px-6 rounded dark:bg-neutral-900 bg-neutral-100">
+      <Link href="/" className="flex items-center gap-2 p-3 px-6 rounded dark:bg-neutral-800 bg-neutral-100">
         <Image src="/app-icon.png" alt="logo" width={28} height={28} />
         <h1 className="text-2xl font-bold">
           Simple Analytics

c6279844f108b266d979c7aac894c208c899e127
Abhishek <abhishekY495.sec@gmail.com>
2026-03-10 12:46:30 +0530

    refactor

--- backend/schema/schema.sql
+++ backend/schema/schema.sql
@@ -15,9 +15,9 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- user sessions
 CREATE TABLE IF NOT EXISTS user_sessions (
-  "id"                 UUID         PRIMARY KEY DEFAULT gen_random_uuid(),
-  "user_id"            UUID         NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  "refresh_token_hash" TEXT         NOT NULL UNIQUE,
+  "id"                 UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
+  "user_id"            UUID        NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  "refresh_token_hash" TEXT        NOT NULL UNIQUE,
   "expires_at"         TIMESTAMPTZ NOT NULL,
   "created_at"         TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );

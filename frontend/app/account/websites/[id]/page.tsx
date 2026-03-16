2071373fada6be2fe3f11b0ca13184b8e1209f46
Abhishek <abhishekY495.sec@gmail.com>
2026-03-16 11:24:04 +0530

    refactor: remove analytics components and API endpoints

--- frontend/app/account/websites/[id]/page.tsx
+++ frontend/app/account/websites/[id]/page.tsx
@@ -1,16 +1,7 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Spinner } from "@/components/ui/spinner";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectSeparator,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { getWebsiteById } from "@/services/website-service";
 import { useAuthStore } from "@/store/auth-store";
 import { useQuery } from "@tanstack/react-query";
@@ -18,30 +9,15 @@ import { useParams, useRouter } from "next/navigation";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { ChevronLeftIcon, ChevronRightIcon, EditIcon } from "lucide-react";
+import { EditIcon } from "lucide-react";
 import { EditWebsiteDialog } from "@/components/website/edit-website-dialog";
 import { getWebsiteIcon } from "@/utils/get-website-icon";
-import Metrics from "@/components/analytics/metrics";
-import { ALL_TIME_START_DATE, DATE_FILTERS } from "@/utils/constants";
-import VisitorsViewsBarChart from "@/components/analytics/visitors-views-bar-chart";
-import PageVisitors from "@/components/analytics/page-visitors";
 
 export default function WebsitePage() {
   const { id } = useParams<{ id: string }>();
   const accessToken = useAuthStore((s) => s.accessToken);
   const router = useRouter();
   const [editOpen, setEditOpen] = useState(false);
-  const [selectedRange, setSelectedRange] = useState("today");
-  const [offset, setOffset] = useState(0);
-
-  const handleRangeChange = (value: string) => {
-    setSelectedRange(value);
-    setOffset(0);
-  };
-
-  const isAllTime = selectedRange === "all time";
-  const isRightDisabled = isAllTime || offset === 0;
-  const isLeftDisabled = isAllTime;
 
   const { data: website, isLoading } = useQuery({
     queryKey: ["website", id],
@@ -49,85 +25,6 @@ export default function WebsitePage() {
   });
 
   const websiteData = website?.data;
-
-  const { startDate, endDate } = useMemo(() => {
-    const now = new Date();
-    let start = new Date(now);
-    let end = new Date(now);
-
-    const startOfTomorrow = new Date(now);
-    startOfTomorrow.setHours(0, 0, 0, 0);
-    startOfTomorrow.setDate(startOfTomorrow.getDate() + 1);
-
-    switch (selectedRange) {
-      case "today": {
-        start = new Date(now);
-        start.setHours(0, 0, 0, 0);
-        start.setDate(start.getDate() + offset);
-        end = new Date(start);
-        end.setDate(end.getDate() + 1);
-        break;
-      }
-      case "last 24 hours": {
-        end = new Date(now.getTime() + offset * 24 * 60 * 60 * 1000);
-        start = new Date(end.getTime() - 24 * 60 * 60 * 1000);
-        break;
-      }
-      case "this week": {
-        const day = now.getDay();
-        const thisSunday = new Date(now);
-        thisSunday.setHours(0, 0, 0, 0);
-        thisSunday.setDate(thisSunday.getDate() - day);
-        start = new Date(thisSunday);
-        start.setDate(start.getDate() + offset * 7);
-        end = new Date(start);
-        end.setDate(end.getDate() + 7);
-        break;
-      }
-      case "this month": {
-        start = new Date(now.getFullYear(), now.getMonth() + offset, 1);
-        end =
-          offset === 0
-            ? startOfTomorrow
-            : new Date(now.getFullYear(), now.getMonth() + offset + 1, 1);
-        break;
-      }
-      case "last 3 months": {
-        end = new Date(now);
-        end.setMonth(end.getMonth() + offset * 3);
-        start = new Date(end);
-        start.setMonth(start.getMonth() - 3);
-        break;
-      }
-      case "last 6 months": {
-        end = new Date(now);
-        end.setMonth(end.getMonth() + offset * 6);
-        start = new Date(end);
-        start.setMonth(start.getMonth() - 6);
-        break;
-      }
-      case "this year": {
-        start = new Date(now.getFullYear() + offset, 0, 1);
-        end =
-          offset === 0
-            ? startOfTomorrow
-            : new Date(now.getFullYear() + offset + 1, 0, 1);
-        break;
-      }
-      case "all time": {
-        start = ALL_TIME_START_DATE;
-        break;
-      }
-      default: {
-        start.setTime(start.getTime() - 24 * 60 * 60 * 1000);
-      }
-    }
-
-    return {
-      startDate: start.toISOString(),
-      endDate: end.toISOString(),
-    };
-  }, [selectedRange, offset]);
 
   useEffect(() => {
     if (!isLoading && !websiteData) {
@@ -167,79 +64,6 @@ export default function WebsitePage() {
               <EditIcon size={18} />
               Edit
             </Button>
-          </div>
-
-          <div className="flex flex-col gap-5">
-            <div className="flex items-center justify-between">
-              <h1 className="text-xl font-semibold">Metrics</h1>
-              <div className="flex items-center gap-2">
-                <div className="flex items-center gap-1">
-                  <Button
-                    variant="outline"
-                    className="rounded px-6 cursor-pointer"
-                    disabled={isLeftDisabled}
-                    onClick={() => setOffset((prev) => prev - 1)}
-                  >
-                    <ChevronLeftIcon size={18} />
-                  </Button>
-                  <Button
-                    variant="outline"
-                    className="rounded px-6 cursor-pointer"
-                    disabled={isRightDisabled}
-                    onClick={() => setOffset((prev) => prev + 1)}
-                  >
-                    <ChevronRightIcon size={18} />
-                  </Button>
-                </div>
-                <Select value={selectedRange} onValueChange={handleRangeChange}>
-                  <SelectTrigger className="w-44 rounded text-[15px] font-medium cursor-pointer">
-                    <SelectValue
-                      placeholder="Last 24 hours"
-                      className="font-medium"
-                    />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectGroup className="rounded">
-                      {DATE_FILTERS.map((filter) => (
-                        <div key={filter.value}>
-                          <SelectItem
-                            className="p-1.5 px-3 rounded text-[15px] cursor-pointer"
-                            value={filter.value}
-                          >
-                            {filter.label}
-                          </SelectItem>
-                          {filter.separator && <SelectSeparator />}
-                        </div>
-                      ))}
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              </div>
-            </div>
-
-            <Metrics
-              websiteId={id}
-              startDate={startDate}
-              endDate={endDate}
-              accessToken={accessToken}
-            />
-
-            <VisitorsViewsBarChart
-              websiteId={id}
-              startDate={startDate}
-              endDate={endDate}
-              accessToken={accessToken}
-              selectedRange={selectedRange}
-            />
-
-            <div className="grid grid-cols-2 gap-4">
-              <PageVisitors
-                websiteId={id}
-                startDate={startDate}
-                endDate={endDate}
-                accessToken={accessToken}
-              />
-            </div>
           </div>
 
           {websiteData && (
--- frontend/utils/constants.ts
+++ frontend/utils/constants.ts
@@ -1,64 +1,6 @@
-import { ChartConfig } from "@/components/ui/chart";
-
 export const API_URL =
   process.env.NEXT_PUBLIC_IS_DEV === "true"
     ? "http://localhost:8080"
     : process.env.BACKEND_URL || "";
 
 export const ICON_BASE_URL = "https://icons.duckduckgo.com/ip3";
-
-export const ALL_TIME_START_DATE = new Date("2026-01-01");
-
-export const DATE_FILTERS = [
-  {
-    label: "Today",
-    value: "today",
-    separator: false,
-  },
-  {
-    label: "Last 24 hours",
-    value: "last 24 hours",
-    separator: true,
-  },
-  {
-    label: "This week",
-    value: "this week",
-    separator: false,
-  },
-  {
-    label: "This month",
-    value: "this month",
-    separator: true,
-  },
-  {
-    label: "Last 3 months",
-    value: "last 3 months",
-    separator: false,
-  },
-  {
-    label: "Last 6 months",
-    value: "last 6 months",
-    separator: true,
-  },
-  {
-    label: "This year",
-    value: "this year",
-    separator: false,
-  },
-  {
-    label: "All time",
-    value: "all time",
-    separator: false,
-  },
-];
-
-export const CHART_CONFIG = {
-  visitors: {
-    label: "Visitors",
-    color: "#0072ff",
-  },
-  views: {
-    label: "Views",
-    color: "#4c9cffd4",
-  },
-} satisfies ChartConfig;
